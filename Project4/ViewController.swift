734223d703d862c2f1aa80c269429dae49862ce5
Andrew Paxson <apaxson@ilm.com>
2019-05-25 00:42:52 -0700

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Andrew Paxson on 2019-05-25.
+//  Copyright © 2019 Andrew Paxson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
